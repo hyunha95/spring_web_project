531a6d6a975597f98feb188c285a1626a4bf79ac
hyunha95 <nohyunha95@gmail.com>
2022-05-26 22:09:39 +0900

    파일 경로 생성 & 이미지 업로드 시 thumbnail 생성

--- ex05/src/main/java/org/zerock/controller/UploadController.java
+++ ex05/src/main/java/org/zerock/controller/UploadController.java
@@ -1,6 +1,12 @@
 package org.zerock.controller;
 
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.UUID;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,6 +15,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.multipart.MultipartFile;
 
 import lombok.extern.log4j.Log4j;
+import net.coobird.thumbnailator.Thumbnailator;
 
 @Controller
 @Log4j
@@ -50,6 +57,15 @@ public class UploadController {
     	
     	String uploadFolder = "C:\\web_projects\\spring_web_project\\ex05\\src\\main\\webapp\\resources\\upload\\temp";
     	
+    	// make folder ----------------
+    	File uploadPath = new File(uploadFolder, getFolder());
+    	log.info("upload path: " + uploadPath);
+    	
+    	if(uploadPath.exists() == false) {
+    		uploadPath.mkdirs();
+    	}
+    	// make yyyy/MM/dd folder
+    	
     	for(MultipartFile multipartFile : uploadFile) {
     		log.info("-------------------------------------");
     		log.info("Upload File Name: " + multipartFile.getOriginalFilename());
@@ -61,15 +77,49 @@ public class UploadController {
     		uploadFileName = uploadFileName.substring(uploadFileName.lastIndexOf("\\") + 1);
     		log.info("only file name: " + uploadFileName);
     		
-    		File saveFile = new File(uploadFolder, uploadFileName);
+    		UUID uuid = UUID.randomUUID();
+    		
+    		uploadFileName = uuid.toString() + "_" + uploadFileName;
+    		
     		
     		try {
+//    			File saveFile = new File(uploadFolder, uploadFileName);
+    			File saveFile = new File(uploadPath, uploadFileName);
     			multipartFile.transferTo(saveFile);
+    			// check image type file
+    			if(checkImageType(saveFile)) {
+    				FileOutputStream thumbnail = new FileOutputStream(new File(uploadPath, "s_" + uploadFileName));
+    				
+    				Thumbnailator.createThumbnail(multipartFile.getInputStream(), thumbnail, 100, 100);
+    				thumbnail.close();
+    			}
     		} catch (Exception e) {
-    			
+    			log.error(e.getMessage());
     		}
     	}
     }
+    
+    private String getFolder() {
+    	SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+    	
+    	Date date = new Date();
+    	
+    	String str = sdf.format(date);
+    	
+    	return str.replace("-", File.separator);
+    }
+    
+    private boolean checkImageType(File file) {
+    	try {
+    		String contentType = Files.probeContentType(file.toPath());
+    		
+    		return contentType.startsWith("image");
+    	} catch (IOException e) {
+    		e.printStackTrace();
+    	}
+    	return false;
+    }
+   
 }
 
 
